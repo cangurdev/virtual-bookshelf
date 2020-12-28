71f878dad502b19dc56ae370d856dee191114f24
cvngur <cangur2398@gmail.com>
2020-12-28 14:25:53 +0300

    added html template

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module virtual-bookshelf
 
 go 1.15
 
-require github.com/gofiber/fiber/v2 v2.3.2 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.3.2
+	github.com/gofiber/template v1.6.6
+)
--- server.go
+++ server.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
 
 func main() {
-	app := fiber.New()
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app := fiber.New(fiber.Config{
+		Views: engine,
 	})
-
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
 	app.Listen(":3000")
 }
