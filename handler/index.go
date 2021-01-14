c6ee9c3c6936300a4a1af50c68e1b0a5d6c1c3b6
cvngur <cangur2398@gmail.com>
2021-01-14 20:58:15 +0300

    edited views

--- handler/index.go
+++ handler/index.go
@@ -11,18 +11,22 @@ var bookService = service.NewBookService(repository.NewBookRepository())
 
 func Index(c *fiber.Ctx) error {
 	id := c.Cookies("username")
+	color := "red"
 	books, err := bookService.GetBookshelf(id)
 	if err != nil {
 		fmt.Print(err)
 	}
 	return c.Render("index", fiber.Map{
-		"books": books,
+		"Color": color,
+		"Books": books,
 	})
 }
 func PostIndex(c *fiber.Ctx) error {
 	query := c.FormValue("query")
 	books := service.Search(query)
+	color := "none"
 	return c.Render("index", fiber.Map{
-		"books": books,
+		"Color": color,
+		"Books": books,
 	})
 }
